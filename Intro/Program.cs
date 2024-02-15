94a0785c2c5389c7a42b2ef377e956a566f75816
İbrahim Halil DEMİR <ibrahimdmr1103@gmail.com>
2024-02-15 18:09:57 +0300

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,18 @@
+﻿using Intro.Business;
+using Intro.Entities;
+
+
+
+CourseManager courseManager = new();
+courseManager.GetAll();
+
+Course[] courses2 = courseManager.GetAll();
+
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+
+
+
+
