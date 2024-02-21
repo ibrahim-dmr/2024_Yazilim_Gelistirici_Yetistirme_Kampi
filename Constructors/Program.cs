8d57f0fc70d0eccc0819324323597392610cf4fa
İbrahim Halil DEMİR <ibrahimdmr1103@gmail.com>
2024-02-22 00:20:40 +0300

    file paths updated

---
+++ Constructors/Program.cs
@@ -0,0 +1,35 @@
+﻿class Constructors
+{
+    static void Main(String[] args)
+    {
+        Customer customer = new Customer { Id = 1, FirstName = "İbrahim Halil", LastName = "Demir", City = "Balıkesir" };
+
+        Customer customer2 = new Customer(2, "Can", "Yücel", "İstanbul");
+
+        Console.WriteLine(customer2.FirstName);
+
+    }
+
+}
+
+class Customer
+{
+    public Customer(int id, string firstName, string lastName, string city)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+    }
+
+
+    public Customer()
+    {
+        Console.WriteLine("Yapıcı blok çalıştı");
+    }
+
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,84 @@
+﻿class Interfaces
+{
+    // interface new'lenemez
+    static void Main(string[] args)
+    {
+
+        // interface'ler onu implemente eden class'ın referansını tutabilirler
+        //IPersonManager customerManager = new CustomerManager();
+        //customerManager.Add();
+        //customerManager.update();
+
+        //IPersonManager employeeManager = new EmployeeManager();
+        //employeeManager.Add();
+        //employeeManager.update();
+
+        ProjectManager  projectManager = new ProjectManager();
+        projectManager.Add(new CustomerManager());
+        
+
+        projectManager.Add(new InternManager());
+
+
+    }
+}
+
+interface IPersonManager
+{
+    // unimplemented operation
+    void Add();
+    void update();
+        
+}
+
+
+// inherits --> class  ------------------ implements --> interface
+class CustomerManager : IPersonManager
+{
+    public void Add()
+    {
+        // Müşteri ekleme kodları
+        Console.WriteLine("Müşteri eklendi");
+    }
+
+    public void update()
+    {
+        Console.WriteLine("Müşteri güncellendi");
+    }
+}
+
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    {
+        // Personel ekleme kodları
+        Console.WriteLine("Personel eklendi");
+    }
+
+    public void update()
+    {
+        Console.WriteLine("Personel güncellendi");
+    }
+}
+
+class InternManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Stajyer eklendi");
+    }
+
+    public void update()
+    {
+        Console.WriteLine("Stajyer güncellendi");
+    }
+}
+
+class ProjectManager
+{
+    public void Add(IPersonManager personManager)
+    {
+        personManager.Add();
+    }
+
+}
